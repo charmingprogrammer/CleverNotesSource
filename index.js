6c653b4e4ab285303be4a9a216e0e50de1f82a2c
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-08-12 20:01:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-//side bar function 
-let sidebar__container = document.getElementById('sidebar__container');
-let menu__options = document.getElementById("menu__options");
-
-function open__sidebar() {
-     sidebar__container.style.width = "100%";
-menu__options.style.display = "flex";
-
+//navigation open 
+function  nev__opener() {
+  document.getElementById("nevContainer").style.left = '0';
+  
 }
-function close__sidebar() {
-     sidebar__container.style.width = "";
-menu__options.style.display = "";
+//navigation close
+function  nev__closer() {
+  document.getElementById("nevContainer").style.left = '';
+  
+  
 }
-//get data form student 
-function saveData() {
-let studentName = document.getElementById("studentName").value;
-
-     document.getElementById("student__name").innerHTML = studentName;
-     
-document.getElementById("sdc").style.display = "none";
-     
-     localStorage.setItem("studentName", studentName);
-     
-localStorage.setItem("sdc", "none");
-
-}
-// show student Name form localStorage.
-document.getElementById("student__name").innerHTML = localStorage.getItem('studentName');
-
-document.getElementById("sdc").style.display = localStorage.getItem("sdc");
-     
